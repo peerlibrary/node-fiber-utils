29bf119de445962876d963814e518c5c9b443fc0
Mitar <mitar.git@tnode.com>
2017-03-05 01:47:51 -0800

    Version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'peerlibrary:fiber-utils',
   summary: "Various fiber utilities",
-  version: '0.7.0',
+  version: '0.7.1',
   git: 'https://github.com/peerlibrary/meteor-fiber-utils.git'
 });
 
